80183df078eff35a883b5b4586eea9e6b44bf9e5
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2020-12-21 15:26:34 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner entrada=new Scanner(System.in);
 		
-		System.out.println("Hello Programación2 ");
+		System.out.println("Hello Programación2 de nuevo! ");
 
   }
 }
